2dfd55091b9ba469fdfe2d255a8f008378ab1578
Mukola <mukola.kutsil22@gmail.com>
2020-04-08 00:14:12 +0300

    add_promise.js

---
+++ promise.js
@@ -0,0 +1,40 @@
+var inputRub = document.getElementsByClassName('ua'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+
+
+    function catchData() {
+
+        return new Promise(function(resolve, reject){
+            let request = new XMLHttpRequest();
+            request.open("GET", "js/current.json");
+        
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send();
+        
+            request.onload = function() {
+                if(request.readyState === 4) {
+                        if(request.status == 200) {
+                            resolve(this.response)
+                        }
+                        else {
+                            reject();
+                        
+                        }
+                }
+            }
+        });
+    };
+
+    catchData()
+    .then(function response () {
+        console.log(response);
+        let data = JSON.parse(response);
+        inputUsd.value = inputRub.value / data.usd;
+    })
+    .then(() => console.log(5000))
+    .catch(() => inputUsd.value = "Щось пішло не так!")
+
+
+});
